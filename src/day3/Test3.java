65183b4dfc5db43bdea5c0c4f78a6732e86e8b57
bwf3 <boweifeng.com>
2021-08-11 16:41:53 +0800

    day3

---
+++ src/day3/Test3.java
@@ -0,0 +1,37 @@
+package day3;
+
+/**
+ * 这个类是测试类
+ * @author 博为峰
+ *
+ */
+public class Test3 {
+
+	/**
+	 * 主函数
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		//单行注释
+		System.out.println("输出");
+		
+		
+		/*
+		 * 多行注释
+		 */
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+
+	}
+	
+	/*
+	 * 方法show
+	 */
+	public void show() {
+		
+		
+	}
+
+}
